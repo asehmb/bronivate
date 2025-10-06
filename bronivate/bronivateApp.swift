1a48d641ae8c5db7ba78485130e02dc8a0b5c9ba
asehmb <130265153+asehmb@users.noreply.github.com>
2025-10-05 21:07:38 -0600

    Initial Commit

---
+++ bronivate/bronivateApp.swift
@@ -0,0 +1,17 @@
+//
+//  bronivateApp.swift
+//  bronivate
+//
+//  Created by Ajay Sehmbi on 2025-10-05.
+//
+
+import SwiftUI
+
+@main
+struct bronivateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bronivateTests/bronivateTests.swift
@@ -0,0 +1,17 @@
+//
+//  bronivateTests.swift
+//  bronivateTests
+//
+//  Created by Ajay Sehmbi on 2025-10-05.
+//
+
+import Testing
+@testable import bronivate
+
+struct bronivateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
